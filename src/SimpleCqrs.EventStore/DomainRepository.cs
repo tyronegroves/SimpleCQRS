03586f055b66c53965e84315b8ce1ea0a1de4f44
tgroves <grovest@digitalev.com>
2010-08-18 23:10:16 -0500

    added domain repository and event store interface

---
+++ src/SimpleCqrs.EventStore/DomainRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using SimpleCqrs.Domain;
+
+namespace SimpleCqrs.EventStore
+{
+    public class DomainRepository
+    {
+        private readonly IEventStore eventStore;
+
+        public DomainRepository(IEventStore eventStore)
+        {
+            this.eventStore = eventStore;
+        }
+
+        public TAggregateRoot GetById<TAggregateRoot>(Guid aggregateRootId) where TAggregateRoot : AggregateRoot, new()
+        {
+            var domainEvents = eventStore.GetAggregateEvents(aggregateRootId);
+            var aggregateRoot = new TAggregateRoot();
+            aggregateRoot.ApplyEvents(domainEvents.ToArray());
+
+            return aggregateRoot;
+        }
+
+        public void Add(AggregateRoot aggregateRoot)
+        {
+            var domainEvents = aggregateRoot.UncommittedEvents;
+            eventStore.Insert(domainEvents.ToArray());
+            aggregateRoot.CommitEvents();
+        }
+    }
+}
---
+++ src/SimpleCqrs.EventStore/IEventStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using SimpleCqrs.Events;
+
+namespace SimpleCqrs.EventStore
+{
+    public interface IEventStore
+    {
+        IEnumerable<IDomainEvent> GetAggregateEvents(Guid aggregateRootId);
+        void Insert(params IDomainEvent[] domainEvents);
+    }
+}
