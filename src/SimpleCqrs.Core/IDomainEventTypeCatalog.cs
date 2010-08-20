6b4e3460ef153497ce8361cd1fd02f245b3acaaf
Tyrone Groves <grovest@digitalev.com>
2010-08-19 23:18:39 -0500

    added logic for mongo db eventstore

---
+++ src/SimpleCqrs.Core/IDomainEventTypeCatalog.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleCqrs.Core
+{
+    public interface IDomainEventTypeCatalog
+    {
+        IEnumerable<Type> GetAllEventTypes();
+    }
+}
