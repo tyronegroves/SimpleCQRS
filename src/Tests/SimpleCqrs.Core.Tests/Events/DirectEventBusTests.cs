95e9e67d836b6e2f15d5242c13408237b4147bc6
Tyrone Groves <grovest@digitalev.com>
2010-08-28 12:28:12 -0500

    added tests for direct event bus

---
+++ src/Tests/SimpleCqrs.Core.Tests/Events/DirectEventBusTests.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMoq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SimpleCqrs.Events;
+
+namespace SimpleCqrs.Core.Tests.Events
+{
+    [TestClass]
+    public class DirectEventBusTests
+    {
+        private AutoMoqer mocker;
+
+        [TestInitialize]
+        public void SetupMocksForAllTests()
+        {
+            mocker = new AutoMoqer();
+        }
+
+        [TestMethod]
+        public void DomainEventHandlerForMyTestEventIsCalledWhenHandlerTypeIsInTypeCatalog()
+        {
+            var serviceLocator = new MockServiceLocator { ResolveFunc = (t) => new MyTestEventHandler() };
+            mocker.GetMock<ITypeCatalog>()
+                .Setup(typeCatalog => typeCatalog.GetGenericInterfaceImplementations(typeof(IHandleDomainEvents<>)))
+                .Returns(new[] { typeof(MyTestEventHandler) });
+
+            var eventBus = CreateEventBus(serviceLocator);
+            var myTestEvent = new MyTestEvent();
+
+            eventBus.PublishEvent(myTestEvent);
+
+            Assert.AreEqual(101, myTestEvent.Result);
+        }
+
+        [TestMethod]
+        public void DomainEventHandlerThatImplementsTwoHandlersAreCalledWhenHandlerTypeIsInTypeCatalog()
+        {
+            var serviceLocator = new MockServiceLocator { ResolveFunc = (t) => new MyTest2EventHandler() };
+            mocker.GetMock<ITypeCatalog>()
+                .Setup(typeCatalog => typeCatalog.GetGenericInterfaceImplementations(typeof(IHandleDomainEvents<>)))
+                .Returns(new[] { typeof(MyTest2EventHandler) });
+
+            var eventBus = CreateEventBus(serviceLocator);
+            var myTestEvent = new MyTestEvent();
+            var myTest2Event = new MyTest2Event();
+
+            eventBus.PublishEvents(new DomainEvent[]{myTestEvent, myTest2Event});
+
+            Assert.AreEqual(102, myTestEvent.Result);
+            Assert.AreEqual(45, myTest2Event.Result);
+        }
+
+        [TestMethod]
+        public void AllEventHandlersAreCalledWhenHandlerTypesAreInTheTypeCatalog()
+        {
+            var serviceLocator = new MockServiceLocator { ResolveFunc = t => Activator.CreateInstance(t) };
+            mocker.GetMock<ITypeCatalog>()
+                .Setup(typeCatalog => typeCatalog.GetGenericInterfaceImplementations(typeof(IHandleDomainEvents<>)))
+                .Returns(new[] {typeof(MyTestEventHandler), typeof(MyTest2EventHandler) });
+
+            var eventBus = CreateEventBus(serviceLocator);
+            var myTestEvent = new MyTestEvent();
+
+            eventBus.PublishEvent(myTestEvent);
+
+            Assert.IsTrue(myTestEvent.MyTestEventHandlerWasCalled);
+            Assert.IsTrue(myTestEvent.MyTest2EventHandlerWasCalled);
+        }
+
+        private IEventBus CreateEventBus(IServiceLocator serviceLocator)
+        {
+            var typeCatalog = mocker.Resolve<ITypeCatalog>();
+            return new DirectEventBus(typeCatalog, serviceLocator);
+        }
+    }
+
+    public class MyTestEventHandler : IHandleDomainEvents<MyTestEvent>
+    {
+        public void Handle(MyTestEvent domainEvent)
+        {
+            domainEvent.Result = 101;
+            domainEvent.MyTestEventHandlerWasCalled = true;
+        }
+    }
+
+    public class MyTest2EventHandler : IHandleDomainEvents<MyTestEvent>, IHandleDomainEvents<MyTest2Event>
+    {
+        public void Handle(MyTest2Event domainEvent)
+        {
+            domainEvent.Result = 45;
+        }
+
+        public void Handle(MyTestEvent domainEvent)
+        {
+            domainEvent.Result = 102;
+            domainEvent.MyTest2EventHandlerWasCalled = true;
+        }
+    }
+
+    public class MyTestEvent : DomainEvent
+    {
+        public virtual int Result { get; set; }
+        public bool MyTestEventHandlerWasCalled { get; set; }
+        public bool MyTest2EventHandlerWasCalled { get; set; }
+    }
+
+    public class MyTest2Event : DomainEvent
+    {
+        public int Result { get; set; }
+    }
+
+    public class MockServiceLocator : IServiceLocator
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Resolve<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Resolve<T>(string key) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Func<Type, object> ResolveFunc { get; set; }
+
+        public object Resolve(Type type)
+        {
+            return ResolveFunc(type);
+        }
+
+        public IList<T> ResolveServices<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register<TInterface>(Type implType) where TInterface : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register<TInterface, TImplementation>() where TImplementation : class, TInterface
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register<TInterface, TImplementation>(string key) where TImplementation : class, TInterface
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(string key, Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(Type serviceType, Type implType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register<TInterface>(TInterface instance) where TInterface : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Release(object instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TService Inject<TService>(TService instance) where TService : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TearDown<TService>(TService instance) where TService : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
