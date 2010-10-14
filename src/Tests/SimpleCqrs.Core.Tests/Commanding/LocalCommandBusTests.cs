87eea144764dd8c25d26db00079b85fb43135162
Tyrone Groves <grovest@digitalev.com>
2010-10-13 23:28:33 -0500

    minor changes

--- src/Tests/SimpleCqrs.Core.Tests/Commanding/LocalCommandBusTests.cs
+++ src/Tests/SimpleCqrs.Core.Tests/Commanding/LocalCommandBusTests.cs
@@ -190,5 +190,10 @@ namespace SimpleCqrs.Core.Tests.Commanding
         {
             throw new NotImplementedException();
         }
+
+        public void Register<Interface>(Func<Interface> factoryMethod) where Interface : class
+        {
+            throw new NotImplementedException();
+        }
     }
 }
