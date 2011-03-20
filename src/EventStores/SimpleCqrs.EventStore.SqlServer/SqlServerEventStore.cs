c268d67694a38b5a1820c88e26636e53606ea586
Darren Cauthon <darren@cauthon.com>
2011-03-20 08:10:44 -0500

    Comment out the existing store.

--- src/EventStores/SimpleCqrs.EventStore.SqlServer/SqlServerEventStore.cs
+++ src/EventStores/SimpleCqrs.EventStore.SqlServer/SqlServerEventStore.cs
@@ -10,127 +10,127 @@ using ServiceStack.Text;
 
 namespace SimpleCqrs.EventStore.SqlServer
 {
-    public class SqlServerEventStore : IEventStore
-    {
-        private IDomainEventSerializer serializer = null;
-        private SqlServerConfiguration configuration = null;
+//    public class SqlServerEventStore : IEventStore
+//    {
+//        private IDomainEventSerializer serializer = null;
+//        private SqlServerConfiguration configuration = null;
 
-        public SqlServerEventStore(string connectionString, IDomainEventSerializer serializer) :
-            this(new SqlServerConfiguration(connectionString), serializer) {
-        }
+//        public SqlServerEventStore(string connectionString, IDomainEventSerializer serializer) :
+//            this(new SqlServerConfiguration(connectionString), serializer) {
+//        }
 
-        public SqlServerEventStore(SqlServerConfiguration configuration, IDomainEventSerializer serializer) {
-            this.serializer = serializer;
-            this.configuration = configuration;
-            Init();
-        }
+//        public SqlServerEventStore(SqlServerConfiguration configuration, IDomainEventSerializer serializer) {
+//            this.serializer = serializer;
+//            this.configuration = configuration;
+//            Init();
+//        }
 
-        public void Init()
-        {
-            var createSql =
-                @"
-IF  not EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[{0}].[{1}]') AND type in (N'U'))
-begin
-create table [{0}].[{1}](
-   EventId int identity not null primary key,
-   EventType nvarchar(255),
-   AggregateRootId uniqueidentifier not null,
-   EventDate datetime not null,
-   Sequence int not null,
-   Serialized nvarchar(max)
-)
-end";
-            using (var connection = new SqlConnection(configuration.ConnectionString))
-            {
-                connection.Open();
-                var sql = string.Format(createSql, configuration.Schema, configuration.TableName);
-                using(var command = new SqlCommand(sql,connection))
-                {
-                    command.ExecuteNonQuery();
-                }
-                connection.Close();
-            }
-        }
+//        public void Init()
+//        {
+//            var createSql =
+//                @"
+//IF  not EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[{0}].[{1}]') AND type in (N'U'))
+//begin
+//create table [{0}].[{1}](
+//   EventId int identity not null primary key,
+//   EventType nvarchar(255),
+//   AggregateRootId uniqueidentifier not null,
+//   EventDate datetime not null,
+//   Sequence int not null,
+//   Serialized nvarchar(max)
+//)
+//end";
+//            using (var connection = new SqlConnection(configuration.ConnectionString))
+//            {
+//                connection.Open();
+//                var sql = string.Format(createSql, configuration.Schema, configuration.TableName);
+//                using(var command = new SqlCommand(sql,connection))
+//                {
+//                    command.ExecuteNonQuery();
+//                }
+//                connection.Close();
+//            }
+//        }
 
 
-        public IEnumerable<DomainEvent> GetEvents(Guid aggregateRootId, int startSequence)
-        {
-            var events = new List<DomainEvent>();
-            var fetchSql = "select eventtype, serialized from {0}.{1} where aggregaterootid = '{2}' and sequence >= {3}";
-            using (var connection = new SqlConnection(configuration.ConnectionString)) {
-                connection.Open();
-                var sql = string.Format(fetchSql, configuration.Schema, configuration.TableName, aggregateRootId,
-                                        startSequence);
-                using (var command = new SqlCommand(sql, connection)) {
-                    using(var reader = command.ExecuteReader())
-                    {
-                        while(reader.Read())
-                        {  
-                            string type = reader["EventType"].ToString();
-                            string serialized = reader["Serialized"].ToString();
+//        public IEnumerable<DomainEvent> GetEvents(Guid aggregateRootId, int startSequence)
+//        {
+//            var events = new List<DomainEvent>();
+//            var fetchSql = "select eventtype, serialized from {0}.{1} where aggregaterootid = '{2}' and sequence >= {3}";
+//            using (var connection = new SqlConnection(configuration.ConnectionString)) {
+//                connection.Open();
+//                var sql = string.Format(fetchSql, configuration.Schema, configuration.TableName, aggregateRootId,
+//                                        startSequence);
+//                using (var command = new SqlCommand(sql, connection)) {
+//                    using(var reader = command.ExecuteReader())
+//                    {
+//                        while(reader.Read())
+//                        {  
+//                            string type = reader["EventType"].ToString();
+//                            string serialized = reader["Serialized"].ToString();
 
-                            events.Add(serializer.Deserialize(Type.GetType(type), serialized));
-                        }
-                    }
-                }
-                connection.Close();
-            }
-            return events;
-        }
+//                            events.Add(serializer.Deserialize(Type.GetType(type), serialized));
+//                        }
+//                    }
+//                }
+//                connection.Close();
+//            }
+//            return events;
+//        }
 
-        public void Insert(IEnumerable<DomainEvent> domainEvents)
-        {
-            var sql = new StringBuilder();
-            foreach (var de in domainEvents)
-            {
-                sql.AppendFormat("insert into {0}.{1} values ('{2}', '{3}', '{4}', {5}, '{6}')",
-                                 configuration.Schema, configuration.TableName,
-                                 de.GetType().AssemblyQualifiedName, de.AggregateRootId, de.EventDate, de.Sequence,
-                                 serializer.Serialize(de));
-            }
+//        public void Insert(IEnumerable<DomainEvent> domainEvents)
+//        {
+//            var sql = new StringBuilder();
+//            foreach (var de in domainEvents)
+//            {
+//                sql.AppendFormat("insert into {0}.{1} values ('{2}', '{3}', '{4}', {5}, '{6}')",
+//                                 configuration.Schema, configuration.TableName,
+//                                 de.GetType().AssemblyQualifiedName, de.AggregateRootId, de.EventDate, de.Sequence,
+//                                 serializer.Serialize(de));
+//            }
 
-            if (sql.Length > 0)
-            {
-                using (var connection = new SqlConnection(configuration.ConnectionString))
-                {
-                    connection.Open();
-                    using (var command = new SqlCommand(sql.ToString(), connection))
-                    {
-                        command.ExecuteNonQuery();
-                    }
-                    connection.Close();
-                }
-            }
-        }
+//            if (sql.Length > 0)
+//            {
+//                using (var connection = new SqlConnection(configuration.ConnectionString))
+//                {
+//                    connection.Open();
+//                    using (var command = new SqlCommand(sql.ToString(), connection))
+//                    {
+//                        command.ExecuteNonQuery();
+//                    }
+//                    connection.Close();
+//                }
+//            }
+//        }
 
-        public IEnumerable<DomainEvent> GetEventsByEventTypes(IEnumerable<Type> domainEventTypes) {
-            var events = new List<DomainEvent>();
+//        public IEnumerable<DomainEvent> GetEventsByEventTypes(IEnumerable<Type> domainEventTypes) {
+//            var events = new List<DomainEvent>();
 
-            string eventParameters = domainEventTypes.Select(x => x.GetType().FullName).Join("','");
+//            string eventParameters = domainEventTypes.Select(x => x.GetType().FullName).Join("','");
 
-            if(!string.IsNullOrWhiteSpace(eventParameters))
-            {
-                eventParameters = "'" + eventParameters + "'";
-            }
+//            if(!string.IsNullOrWhiteSpace(eventParameters))
+//            {
+//                eventParameters = "'" + eventParameters + "'";
+//            }
 
-            var fetchSql = "select eventtype, serialized from {0}.{1} where eventtype in '{2}'";
-            using (var connection = new SqlConnection(configuration.ConnectionString)) {
-                connection.Open();
-                var sql = string.Format(fetchSql, configuration.Schema, configuration.TableName, eventParameters);
-                using (var command = new SqlCommand(sql, connection)) {
-                    using (var reader = command.ExecuteReader()) {
-                        while (reader.Read()) {
-                            string type = reader["EventType"].ToString();
-                            string serialized = reader["Serialized"].ToString();
+//            var fetchSql = "select eventtype, serialized from {0}.{1} where eventtype in '{2}'";
+//            using (var connection = new SqlConnection(configuration.ConnectionString)) {
+//                connection.Open();
+//                var sql = string.Format(fetchSql, configuration.Schema, configuration.TableName, eventParameters);
+//                using (var command = new SqlCommand(sql, connection)) {
+//                    using (var reader = command.ExecuteReader()) {
+//                        while (reader.Read()) {
+//                            string type = reader["EventType"].ToString();
+//                            string serialized = reader["Serialized"].ToString();
 
-                            events.Add(serializer.Deserialize(Type.GetType(type), serialized));
-                        }
-                    }
-                }
-                connection.Close();
-            }
-            return events;
-        }
+//                            events.Add(serializer.Deserialize(Type.GetType(type), serialized));
+//                        }
+//                    }
+//                }
+//                connection.Close();
+//            }
+//            return events;
+//        }
 
-    }
+//    }
 }
