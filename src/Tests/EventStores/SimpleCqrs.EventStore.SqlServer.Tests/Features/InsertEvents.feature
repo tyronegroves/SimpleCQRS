9248621719692b130bb2e414479b9ab3d99ebc4d
Darren Cauthon <darren@cauthon.com>
2011-03-20 08:50:25 -0500

    Get the first specs running.

---
+++ src/Tests/EventStores/SimpleCqrs.EventStore.SqlServer.Tests/Features/InsertEvents.feature
@@ -0,0 +1,23 @@
+﻿Feature: Insert events
+	In order to add events to my SQL Server event store
+	As a Simple CQRS developer
+	I want to pass an array of events and have them added to the appropriate table
+
+# Special Note:  I think it's probably better to test the final results 
+# in the SQL server, for now I'm just going to test the SQL statements
+# that are to be run against the DB.  I think it's worth demonstrating 
+# that you can bend these types of specs to fit what you want (instead
+# of just throwing away this type of testing alltogether).
+
+Scenario: Insert one domain event
+	Given I have a SomethingHappenedEvent to be added to the store with the following values
+	| Field           | Value                                |
+	| EventDate       | 3/20/2010 3:01:04 AM                 |
+	| AggregateRootId | 8312E92C-DF1C-4970-A9D5-6414120C3CF7 |
+	| Sequence        | 2                                    |
+	When I add the domain events to the store
+	Then the following SQL statement should be run
+	"""
+	Insert into Event_Store (EventType, AggregateRootId, EventDate, Sequence) 
+	  Values ('SomethingHappened', '8312E92C-DF1C-4970-A9D5-6414120C3CF7', '3/20/2010 3:01:04 AM' ,'2');
+	"""
---
+++ src/Tests/EventStores/SimpleCqrs.EventStore.SqlServer.Tests/Specs/SomethingHappenedEventSteps.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using Moq;
+using SimpleCqrs.Eventing;
+using SimpleCqrs.Unity;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace SimpleCqrs.EventStore.SqlServer.Tests.Specs
+{
+    [Binding]
+    public class RuntimeSteps
+    {
+        public static TestingRuntime Runtime;
+
+        [BeforeTestRun]
+        public static void TestRun()
+        {
+            Runtime = new TestingRuntime();
+            Runtime.Start();
+        }
+
+        [AfterTestRun]
+        public static void Cleanup()
+        {
+            Runtime.Shutdown();
+        }
+
+        [BeforeScenario]
+        public void Setup()
+        {
+            ScenarioContext.Current.Set<IServiceLocator>(Runtime.ServiceLocator);
+        }
+
+        [AfterScenario]
+        public void Teardown()
+        {
+        }
+    }
+
+    public class TestingRuntime : SimpleCqrsRuntime<UnityServiceLocator>
+    {
+    }
+
+    [Binding]
+    public class SqlServerEventStoreSteps
+    {
+        [When(@"I add the domain events to the store")]
+        public void WhenIAddTheDomainEventsToTheStore()
+        {
+            var eventsToAdd = ScenarioContext.Current.Get<IEnumerable<DomainEvent>>();
+
+            var eventStore = CreateTheEventStore();
+            eventStore.Insert(eventsToAdd);
+        }
+
+        private SqlServerEventStore CreateTheEventStore()
+        {
+            return ScenarioContext.Current.Get<IServiceLocator>().Resolve<SqlServerEventStore>();
+        }
+    }
+
+    public class SqlServerEventStore : IEventStore
+    {
+        public IEnumerable<DomainEvent> GetEvents(Guid aggregateRootId, int startSequence)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(IEnumerable<DomainEvent> domainEvents)
+        {
+            
+        }
+
+        public IEnumerable<DomainEvent> GetEventsByEventTypes(IEnumerable<Type> domainEventTypes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [Binding]
+    public class SqlStatementRunner
+    {
+        private Mock<ISqlStatementRunner> fakeSqlStatementRunner;
+
+        [BeforeScenario]
+        public void Setup()
+        {
+            fakeSqlStatementRunner = new Mock<ISqlStatementRunner>();
+            RuntimeSteps.Runtime.ServiceLocator.Register(fakeSqlStatementRunner.Object);
+        }
+
+        [Then(@"the following SQL statement should be run")]
+        public void x(string expectedSqlStatement)
+        {
+            fakeSqlStatementRunner.Verify(x=>x.RunThisSql(expectedSqlStatement), Times.Once());
+        }
+    }
+
+    public interface ISqlStatementRunner
+    {
+        void RunThisSql(string sqlStatement);
+    }
+
+    [Binding]
+    public class DomainEventSteps
+    {
+        private IList<DomainEvent> eventsToAdd;
+
+        [BeforeScenario]
+        public void Setup()
+        {
+            eventsToAdd = new List<DomainEvent>();
+            ScenarioContext.Current.Set<IEnumerable<DomainEvent>>(() => eventsToAdd);
+        }
+
+        [Given(@"I have a SomethingHappenedEvent to be added to the store with the following values")]
+        public void x(Table table)
+        {
+            var somethingHappenedEvent = table.CreateInstance<SomethingHappenedEvent>();
+            eventsToAdd.Add(somethingHappenedEvent);
+        }
+    }
+
+    public class SomethingHappenedEvent : DomainEvent
+    {
+    }
+}
