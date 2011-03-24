9c45fabaa3238311df7b7077204448a062ebb870
Tyrone Groves <grovest@digitalev.com>
2011-03-24 17:20:35 -0500

    added entity and entity collection

---
+++ src/SimpleCqrs/Domain/EntityCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace SimpleCqrs.Domain
+{
+    public class EntityCollection<TEntity> : Collection<TEntity> where TEntity : Entity
+    {
+        private readonly AggregateRoot aggregateRoot;
+
+        public EntityCollection(AggregateRoot aggregateRoot)
+        {
+            this.aggregateRoot = aggregateRoot;
+        }
+
+        public TEntity FindById(Guid id)
+        {
+            return this.SingleOrDefault(entity => entity.Id == id);
+        }
+
+        protected override void InsertItem(int index, TEntity entity)
+        {
+            aggregateRoot.RegisterEntity(entity);
+            base.InsertItem(index, entity);
+        }
+    }
+}
---
+++ src/SimpleCqrs/Eventing/EntityDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SimpleCqrs.Eventing
+{
+    public class EntityDomainEvent : DomainEvent
+    {
+        public Guid EntityId { get; set; }
+    }
+}
