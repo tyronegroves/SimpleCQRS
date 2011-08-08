40d2521df46b723973f7d0ccc83e0f34542e350b
Darren Cauthon <darren@cauthon.com>
2011-08-07 20:19:20 -0500

    Refactors.

--- src/EventStores/SimpleCqrs.EventStore.SqlServer/DomainEventTypesDictionaryGenerator.cs
+++ src/EventStores/SimpleCqrs.EventStore.SqlServer/DomainEventTypesDictionaryGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using SimpleCqrs.Eventing;
 
 namespace SimpleCqrs.EventStore.SqlServer
@@ -9,13 +10,32 @@ namespace SimpleCqrs.EventStore.SqlServer
     {
         public IDictionary<string, Type> GenerateDictionaryOfDomainTypes()
         {
-            var list = new List<Type>();
-            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                var domainEventTypes = assembly.GetTypes().Where(x => x.BaseType == typeof (DomainEvent));
-                list.AddRange(domainEventTypes);
-            }
+            var list = GetAListOfAllDomainEventTypes();
             return list.ToDictionary(x => x.Name, x => x);
+        }
+
+        private IEnumerable<Type> GetAListOfAllDomainEventTypes()
+        {
+            return AppDomain.CurrentDomain.GetAssemblies()
+                .Select(GetTheDomainEventTypesFromThisAssembly)
+                .SelectMany(x => x);
+        }
+
+        private static IEnumerable<Type> GetTheDomainEventTypesFromThisAssembly(Assembly assembly)
+        {
+            try
+            {
+                return MakeACallToGetTheTypesFromTheAssemblyInAWayThatCouldPotentiallyThrow(assembly);
+            }
+            catch
+            {
+                return new Type[] {};
+            }
+        }
+
+        private static IEnumerable<Type> MakeACallToGetTheTypesFromTheAssemblyInAWayThatCouldPotentiallyThrow(Assembly assembly)
+        {
+            return assembly.GetTypes().Where(x => x.BaseType == typeof (DomainEvent));
         }
     }
 }
