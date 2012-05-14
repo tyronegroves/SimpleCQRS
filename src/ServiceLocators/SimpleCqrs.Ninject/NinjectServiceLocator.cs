1fea8345ca215df521cabf1de74454fd05f9567f
Juan Pablo Olmos Lara <jupaoljpol@gmail.com>
2012-05-14 06:41:11 -0500

    Ninject Service Locator with tests added

---
+++ src/ServiceLocators/SimpleCqrs.Ninject/NinjectServiceLocator.cs
@@ -0,0 +1,287 @@
+﻿// -----------------------------------------------------------------------
+// https://github.com/jupaol
+// jupaol@hotmail.com
+// http://jupaol.blogspot.com/
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ninject;
+
+namespace SimpleCqrs.Ninject
+{
+    /// <summary>
+    /// Ninject service locator
+    /// </summary>
+    public class NinjectServiceLocator : IServiceLocator
+    {
+        /// <summary>
+        /// Ninject kernel used as the current IoC container
+        /// </summary>
+        private IKernel kernel;
+
+        /// <summary>
+        /// Error message used when the type specified in the register methods is null
+        /// </summary>
+        private const string TypeNullErrorMessage = "The container does not accept null types to be registered";
+
+        /// <summary>
+        /// Error message used when the key specified in the register methods is null, empty or a string with white spaces only
+        /// </summary>
+        private const string KeyNullErrorMessage = "The key cannot be null, empty or a string with white spaces only";
+
+        /// <summary>
+        /// Error message used when the kernel is null
+        /// </summary>
+        private const string KernelNullErrorMessage = "The specified Ninject kernel cannot be null";
+
+        /// <summary>
+        /// Error message used when the reset method is called, actually it is not supported because Ninject does not suppor reseting the container
+        /// </summary>
+        private const string ResetingNinjectContainerErrorMessage = "Ninject does not support reseting the container";
+
+        /// <summary>
+        /// Error message used when the instance passed to the register methods is null
+        /// </summary>
+        private const string InstanceNullErrorMessage = "Null objects cannot be registered in the container";
+
+        /// <summary>
+        /// Error message used when the calling delegate passed to the register methods is null
+        /// </summary>
+        private const string CallingDelegateNullErrorMessage = "The calling delegate connot be null";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NinjectServiceLocator"/> class.
+        /// </summary>
+        /// <remarks>
+        /// It creates a new instance of the <see cref="StandardKernel"/> to initialize this instance
+        /// </remarks>
+        public NinjectServiceLocator()
+            : this(new StandardKernel())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NinjectServiceLocator"/> class.
+        /// </summary>
+        /// <param name="kernel">The Ninject kernel.</param>
+        public NinjectServiceLocator(IKernel kernel)
+        {
+            if (kernel == null)
+            {
+                throw new ArgumentNullException("kernel", KernelNullErrorMessage);
+            }
+
+            this.IsDisposed = false;
+            this.kernel = kernel;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is disposed.
+        /// </summary>
+        /// <value>
+        /// 	<c>true</c> if this instance is disposed; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsDisposed { get; protected set; }
+
+        public T Resolve<T>() where T : class
+        {
+            try
+            {
+                return this.kernel.Get<T>();
+            }
+            catch (Exception ex)
+            {
+                throw new ServiceResolutionException(typeof(T), ex);
+            }
+        }
+
+        public T Resolve<T>(string key) where T : class
+        {
+            try
+            {
+                return this.kernel.Get<T>(key);
+            }
+            catch (Exception ex)
+            {
+                throw new ServiceResolutionException(typeof(T), ex);
+            }
+        }
+
+        public object Resolve(Type type)
+        {
+            try
+            {
+                return this.kernel.Get(type);
+            }
+            catch (Exception ex)
+            {
+                throw new ServiceResolutionException(typeof(Type), ex);
+            }
+        }
+
+        public IList<T> ResolveServices<T>() where T : class
+        {
+            try
+            {
+                return this.kernel.GetAll<T>().ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new ServiceResolutionException(typeof(T), ex);
+            }
+        }
+
+        public void Register<TInterface>(Type implType) where TInterface : class
+        {
+            if (implType == null)
+            {
+                throw new ArgumentNullException("implType", TypeNullErrorMessage);
+            }
+
+            this.kernel.Bind<TInterface>().To(implType);
+        }
+
+        public void Register<TInterface, TImplementation>() where TImplementation : class, TInterface
+        {
+            this.kernel.Bind<TInterface>().To<TImplementation>();
+        }
+
+        public void Register<TInterface, TImplementation>(string key) where TImplementation : class, TInterface
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentNullException("key", KeyNullErrorMessage);
+            }
+
+            this.kernel.Bind<TInterface>().To<TImplementation>().Named(key);
+        }
+
+        public void Register(string key, Type type)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentNullException("key", KeyNullErrorMessage);
+            }
+
+            if (type == null)
+            {
+                throw new ArgumentNullException("implType", TypeNullErrorMessage);
+            }
+
+            this.kernel.Bind(type).ToSelf().Named(key);
+        }
+
+        public void Register(Type serviceType, Type implType)
+        {
+            if (serviceType == null)
+            {
+                throw new ArgumentNullException("serviceType", TypeNullErrorMessage);
+            }
+
+            if (implType == null)
+            {
+                throw new ArgumentNullException("implType", TypeNullErrorMessage);
+            }
+
+            this.kernel.Bind(serviceType).To(implType);
+        }
+
+        public void Register<TInterface>(TInterface instance) where TInterface : class
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance", InstanceNullErrorMessage);
+            }
+
+            this.kernel.Bind<TInterface>().ToConstant(instance);
+        }
+
+        public void Release(object instance)
+        {
+            if (instance == null)
+            {
+                return;
+            }
+
+            // TODO: verify call
+            this.kernel.Release(instance);
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException(ResetingNinjectContainerErrorMessage);
+        }
+
+        public TService Inject<TService>(TService instance) where TService : class
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance", InstanceNullErrorMessage);
+            }
+
+            this.kernel.Inject(instance);
+            return instance;
+        }
+
+        public void TearDown<TService>(TService instance) where TService : class
+        {
+            if (instance == null)
+            {
+                return;
+            }
+
+            // TODO: verify call
+            this.kernel.Release(instance);
+        }
+
+        public void Register<TInterface>(Func<TInterface> factoryMethod) where TInterface : class
+        {
+            if (factoryMethod == null)
+            {
+                throw new ArgumentNullException("factoryMethod", CallingDelegateNullErrorMessage);
+            }
+
+            TInterface theInstance = factoryMethod();
+
+            if (theInstance == null)
+            {
+                throw new ArgumentNullException("factoryMethod", TypeNullErrorMessage);
+            }
+
+            this.kernel.Bind<TInterface>().ToMethod(x => theInstance);
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.IsDisposed)
+            {
+                if (disposing)
+                {
+                    if (this.kernel != null)
+                    {
+                        if (!this.kernel.IsDisposed)
+                        {
+                            this.kernel.Dispose();
+                        }
+                    }
+                }
+
+                this.IsDisposed = true;
+            }
+        }
+    }
+}
---
+++ src/Tests/ServiceLocators/SimpleCqrs.Ninject.Tests/Integration/NinjectServiceLocatorIntegrationTests.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using SimpleCqrs.Commanding;
+using SimpleCqrs.Domain;
+using SimpleCqrs.Eventing;
+
+namespace SimpleCqrs.Ninject.Tests.Integration
+{
+    [TestClass]
+    public class NinjectServiceLocatorIntegrationTests
+    {
+        [TestMethod]
+        public void can_create_a_new_instance_of_the_Simple_CQRS_Runtime()
+        {
+            var sut = new MySimpleCqrsRuntime();
+            sut.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void can_start_and_stop_a_new_instance_of_MySimpleRuntime()
+        {
+            var sut = new MySimpleCqrsRuntime();
+
+            sut.Invoking(x => sut.Start()).ShouldNotThrow();
+            sut.Invoking(x => x.Shutdown()).ShouldNotThrow();
+        }
+
+        [TestMethod]
+        public void can_send_a_simple_command_and_firing_domain_events_happy_path()
+        {
+            using (var runtime = new MySimpleCqrsRuntimeHelper().Build())
+            {
+                var command = new CreateAccountCommand { FirstName = "Juan", LastName = "Olmos" };
+                var commandBus = runtime.ServiceLocator.Resolve<ICommandBus>();
+
+                commandBus.Send(command);
+
+                CreateAccountCommandHandlerWasCalled.Should().BeTrue();
+                CreateAccountInternalEventHandlerWasCalled.Should().BeTrue();
+                NameSetInternalEventHandlerWasCalled.Should().BeTrue();
+                CreateAccountEventHandlerWasCalled.Should().BeTrue();
+                NameSetEventHandlerWasCalled.Should().BeTrue();
+            }
+        }
+
+        internal static bool CreateAccountCommandHandlerWasCalled = false;
+        internal static bool CreateAccountInternalEventHandlerWasCalled = false;
+        internal static bool NameSetInternalEventHandlerWasCalled = false;
+        internal static bool CreateAccountEventHandlerWasCalled = false;
+        internal static bool NameSetEventHandlerWasCalled = false;
+    }
+
+    class MySimpleCqrsRuntimeHelper : IDisposable
+    {
+        public MySimpleCqrsRuntime Runtime { get; private set; }
+
+        public MySimpleCqrsRuntimeHelper()
+        {
+            this.Runtime = new MySimpleCqrsRuntime();
+            this.Runtime.Start();
+        }
+
+        public void Dispose()
+        {
+            this.Runtime.Shutdown();
+            this.Runtime.Dispose();
+            this.Dispose();
+        }
+
+        public MySimpleCqrsRuntime Build()
+        {
+            return this.Runtime;
+        }
+    }
+
+    class MySimpleCqrsRuntime : SimpleCqrsRuntime<NinjectServiceLocator>
+    {
+    }
+
+    class CreateAccountCommand : ICommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class CreateAccountCommandHandler : CommandHandler<CreateAccountCommand>
+    {
+        private IDomainRepository repository;
+
+        public CreateAccountCommandHandler(IDomainRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public override void Handle(CreateAccountCommand command)
+        {
+            NinjectServiceLocatorIntegrationTests.CreateAccountCommandHandlerWasCalled = true;
+
+            command.Should().NotBeNull();
+            command.FirstName.Should().Be("Juan");
+            command.LastName.Should().Be("Olmos");
+
+            var account = new Account(Guid.NewGuid());
+
+            account.SetName(command.FirstName, command.LastName);
+
+            this.repository.Save(account);
+        }
+    }
+
+    class Account : AggregateRoot
+    {
+        public Account(Guid id)
+        {
+            this.Apply(new AccountCreatedEvent(id));
+        }
+
+        public void SetName(string firstName, string lastName)
+        {
+            this.Apply(new AccountNameSetEvent(firstName, lastName));
+        }
+
+        public void OnAccountCreated(AccountCreatedEvent e)
+        {
+            NinjectServiceLocatorIntegrationTests.CreateAccountInternalEventHandlerWasCalled = true;
+
+            e.Should().NotBeNull();
+            e.Id.Should().NotBeEmpty();
+        }
+
+        public void OnAccountNameSet(AccountNameSetEvent e)
+        {
+            NinjectServiceLocatorIntegrationTests.NameSetInternalEventHandlerWasCalled = true;
+
+            e.Should().NotBeNull();
+            e.FirstName.Should().Be("Juan");
+            e.LastName.Should().Be("Olmos");
+        }
+    }
+
+    class AccountNameSetEvent : DomainEvent
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public AccountNameSetEvent(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+
+    class AccountCreatedEvent : DomainEvent
+    {
+        public Guid Id { get; private set; }
+
+        public AccountCreatedEvent(Guid id)
+        {
+            this.Id = id;
+        }
+    }
+
+    class AccountReportDenormalized : IHandleDomainEvents<AccountCreatedEvent>, IHandleDomainEvents<AccountNameSetEvent>
+    {
+
+        public void Handle(AccountNameSetEvent domainEvent)
+        {
+            NinjectServiceLocatorIntegrationTests.NameSetEventHandlerWasCalled = true;
+        }
+
+        public void Handle(AccountCreatedEvent domainEvent)
+        {
+            NinjectServiceLocatorIntegrationTests.CreateAccountEventHandlerWasCalled = true;
+        }
+    }
+}
